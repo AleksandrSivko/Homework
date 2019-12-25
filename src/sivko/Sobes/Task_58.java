02864b6fc33e5eaab3b254b4d813bb0467240238
AleksandrSivko <Mr.seva@tut.by>
2019-12-25 13:43:39 +0300

    Sobes final

--- src/sivko/Sobes/Task_58.java
+++ src/sivko/Sobes/Task_58.java
@@ -6,13 +6,49 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Task_58 {
-        public static void main(String[] argv) throws IOException{new Task_58().run();}
-        public void run() throws IOException {
-            Scanner sc = new Scanner(new File("INPUT.TXT"));
-            int a = sc.nextInt();
-
-            PrintWriter pw = new PrintWriter(new File("OUTPUT.TXT"));
-            pw.print(a);
-            pw.close();
-        }
+    public static void main(String[] argv) throws IOException {
+        new Task_58().run();
     }
+
+    public void run() throws IOException {
+        Scanner sc = new Scanner(new File("INPUT.TXT"));
+        PrintWriter pw = new PrintWriter(new File("OUTPUT.TXT"));
+        int nambTab = sc.nextInt();
+        for (int k = 1; k <= nambTab; k++) {
+            System.out.println(k);
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int[][] tabl = new int[b][c];
+            if (b == 1 || c == 1) {
+                pw.println("YES");
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < c; j++) {
+                        tabl[i][j] = sc.nextInt();
+                    }
+                }
+                continue;
+            } else {
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < c; j++) {
+                        tabl[i][j] = sc.nextInt();
+                    }
+                }
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < c; j++) {
+                    }
+                }
+            }
+            String res = "";
+            rr:
+            for (int i = 0; i < b - 1; i++) {
+                for (int j = 0; j < c - 1; j++) {
+                    res = ((tabl[i][j] != tabl[i][j + 1]) || (tabl[i][j] != tabl[i + 1][j]) || (tabl[i][j] != tabl[i + 1][j + 1]) ? "YES" : "NO");
+                    if(res == "NO") break rr;
+                }
+            }
+            System.out.println(res);
+            pw.println(res);
+        }
+        pw.close();
+    }
+}
